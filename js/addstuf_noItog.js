984448568b175b92276eb85435b868d82796011c
DimaZUA <60350224+DimaZUA@users.noreply.github.com>
2024-11-14 01:58:02 +0200

    Add files via upload

--- js/addstuf_noItog.js
+++ js/addstuf_noItog.js
@@ -10,8 +10,31 @@
     }
 
     let cumulativeBalance = 0;
-    let currentMonth = new Date().getMonth(); // Текущий месяц
-    let currentYear = new Date().getFullYear();
+
+// Добавляем оплату за те месяцы и годы, которые отсутствуют в начислениях
+for (const year in paymentData) {
+    if (!accountData[year]) {
+        // Если год отсутствует в начислениях, добавляем все оплаты за этот год
+        for (const month in paymentData[year]) {
+            const monthlyPayments = paymentData[year][month] || [];
+            // Суммируем все платежи за этот месяц
+            const totalPayments = monthlyPayments.reduce((sum, payment) => sum + payment.sum, 0);
+            cumulativeBalance -= totalPayments; // Учитываем оплату как уменьшение долга (или увеличение переплаты)
+        }
+    } else {
+        // Если год есть в начислениях, проверяем каждый месяц
+        for (const month in paymentData[year]) {
+            if (!accountData[year][month]) {
+                // Если месяц отсутствует в начислениях, добавляем оплату только за этот месяц
+                const monthlyPayments = paymentData[year][month] || [];
+                // Суммируем все платежи за этот месяц
+                const totalPayments = monthlyPayments.reduce((sum, payment) => sum + payment.sum, 0);
+                cumulativeBalance -= totalPayments; // Учитываем оплату как уменьшение долга (или увеличение переплаты)
+            }
+        }
+    }
+}
+    
     let lastYearToggle; // Переменная для хранения чекбокса последнего года
 
     for (const year in accountData) {
@@ -29,37 +52,37 @@
         yearLabel.innerHTML = `<div>${year}</div>`;
 
         yearContent.className = 'box';
-        if (cumulativeBalance !== 0) {
-            // Текст в зависимости от значения cumulativeBalance
-            const balanceText = cumulativeBalance > 0
-                ? '⚠️ Вхідний борг на початок року'
-                : '✅ Вхідна переплата на початок року';
+if (cumulativeBalance !== 0) {
+    // Текст в зависимости от значения cumulativeBalance
+    const balanceText = cumulativeBalance > 0
+        ? '⚠️ Вхідний борг на початок року'
+        : '✅ Вхідна переплата на початок року';
 
-            // Создаем элемент для блока с информацией
-            const balanceDiv = document.createElement('div');
-            balanceDiv.className = 'balance-info';
+    // Создаем элемент для блока с информацией
+    const balanceDiv = document.createElement('div');
+    balanceDiv.className = 'balance-info';
 
-            // Создаем span для числа
-            let balanceValue = document.createElement('span');
-            balanceValue.textContent = `${cumulativeBalance.toFixed(2)}`;
+    // Создаем span для числа
+    let balanceValue = document.createElement('span');
+    balanceValue.textContent = `${cumulativeBalance.toFixed(2)}`;
 
-            // Если число положительное, делаем его красным
-            if (cumulativeBalance > 0) {
-                balanceValue.classList.add("red"); // Красный цвет для положительного значения
-            } else {
-                balanceValue.classList.add('green'); // Зеленый цвет для отрицательного значения
-            }
+    // Если число положительное, делаем его красным
+    if (cumulativeBalance > 0) {
+        balanceValue.style.color = 'red'; // Красный цвет для положительного значения
+    } else {
+        balanceValue.style.color = 'green'; // Зеленый цвет для отрицательного значения
+    }
 
-            // Добавляем текст и число в блок
-            balanceDiv.textContent = `${balanceText}: `;
-            balanceDiv.appendChild(balanceValue);
-            balanceValue = document.createElement('span');
-            balanceValue.textContent = ' грн.';
-            balanceDiv.appendChild(balanceValue);
+    // Добавляем текст и число в блок
+    balanceDiv.textContent = `${balanceText}: `;
+    balanceDiv.appendChild(balanceValue);
+    balanceValue=document.createElement('span');
+    balanceValue.textContent=' грн.';
+    balanceDiv.appendChild(balanceValue);
+    // Вставляем блок с информацией перед таблицей
+    yearContent.appendChild(balanceDiv);
+}
 
-            // Вставляем блок с информацией перед таблицей
-            yearContent.appendChild(balanceDiv);
-        }
 
         const table = document.createElement('table');
         table.id = 'main';
@@ -76,11 +99,12 @@
 
         // Заголовок таблицы
         const headerRow = document.createElement('tr');
-        headerRow.innerHTML =
-            `<td rowspan="2" align="CENTER">Месяц</td>
-             <td colspan="${services.size}" align="CENTER">Начислено за месяц</td>
-             <td rowspan="2" align="CENTER">Оплачено в месяце</td>
-             <td rowspan="2" align="CENTER">Долг(+) Переплата(-) на конец месяца</td>`;
+        headerRow.innerHTML = `
+            <td rowspan="2" align="CENTER">Месяц</td>
+            <td colspan="${services.size}" align="CENTER">Начислено за месяц</td>
+            <td rowspan="2" align="CENTER">Оплачено в месяце</td>
+            <td rowspan="2" align="CENTER">Долг(+) Переплата(-) на конец месяца</td>
+        `;
         tbody.appendChild(headerRow);
 
         // Второй ряд заголовка с названиями услуг
@@ -94,28 +118,18 @@
         });
         tbody.appendChild(servicesRow);
 
-        // Переменные для итоговых сумм по году
-        let totalChargesByService = {};
-        let totalPaymentsByService = {};
-        let totalChargeForYear = 0;
-        let totalPaymentsForYear = 0;
-        let lastRow;
+
         // Заполнение таблицы данными по месяцам
         for (const month in accountData[year]) {
-        
-			 // Пропускаем текущий месяц
-
             const transactions = accountData[year][month];
             const row = document.createElement('tr');
             row.innerHTML = `<td align="LEFT">${getMonthName(month)} ${year}</td>`;
 
             let rowCharges = {};
-            let monthlyChargesTotal = 0;
 
             services.forEach(serviceId => {
                 const charge = transactions[serviceId] || 0;
                 rowCharges[serviceId] = charge;
-                monthlyChargesTotal += charge;
             });
 
             // Добавляем начисления по каждой услуге в строку
@@ -125,103 +139,20 @@
                 cell.setAttribute('v', charge);
                 row.appendChild(cell);
             });
-            const cur=(month == currentMonth+1 && year==currentYear);
+
             // Получаем данные оплат за текущий месяц
             const monthlyPayments = paymentData[year]?.[month] || [];
             const totalPayments = createPaymentCell(row, monthlyPayments);
-            if (!cur){
-            cumulativeBalance += monthlyChargesTotal - totalPayments;
-            // Сохраняем суммы для итогов
-            services.forEach(serviceId => {
-                totalChargesByService[serviceId] = (totalChargesByService[serviceId] || 0) + rowCharges[serviceId];
-            });
-            totalPaymentsForYear += totalPayments;
-             }
+
+            cumulativeBalance += Object.values(rowCharges).reduce((sum, charge) => sum + charge, 0) - totalPayments;
+
             // Добавляем ячейку с долгом/переплатой
             const balanceCell = document.createElement('td');
-            if (cur){
-            	balanceCell.setAttribute('v', (cumulativeBalance+monthlyChargesTotal - totalPayments).toFixed(2));
-            	if (cumulativeBalance+monthlyChargesTotal - totalPayments>0) balanceCell.classList.add("red"); else balanceCell.classList.add("green");
-
-            }else{
-            	balanceCell.setAttribute('v', cumulativeBalance.toFixed(2));
-            	if (cumulativeBalance>0) balanceCell.classList.add("red"); else balanceCell.classList.add("green");
-            }
+            balanceCell.setAttribute('v', cumulativeBalance.toFixed(2));
             row.appendChild(balanceCell);
 
-            if (cur) 
-            {
-            	row.classList.add('grey');
-            	lastRow=row;
-            }else{
-            	tbody.appendChild(row);
-            }
-
+            tbody.appendChild(row);
         }
-
-        // Итоги по году
-if (services.size > 1) {
-    // Если несколько услуг
-    const totalRow = document.createElement('tr');
-    totalRow.classList.add('itog'); 
-    totalRow.innerHTML = `<td rowspan="2" align="CENTER">Итого за ${year} год</td>`;
-
-    // Итог по каждой услуге
-    services.forEach(serviceId => {
-        const chargeTotal = totalChargesByService[serviceId] || 0;
-        const totalCell = document.createElement('td');
-        totalCell.setAttribute('v', chargeTotal.toFixed(2));  
-        totalRow.appendChild(totalCell);
-    });
-
-    // Общая сумма оплаченных денег
-    const totalPaymentsCell = document.createElement('td');
-    totalPaymentsCell.rowSpan = 2;
-    totalPaymentsCell.setAttribute('v', totalPaymentsForYear.toFixed(2));
-    totalRow.appendChild(totalPaymentsCell);
-
-    // Общий долг/переплата на конец года
-    const finalBalanceCell = document.createElement('td');
-    finalBalanceCell.rowSpan = 2;
-    if (cumulativeBalance>0) finalBalanceCell.classList.add("red"); else finalBalanceCell.classList.add("green");
-    finalBalanceCell.setAttribute('v', cumulativeBalance.toFixed(2));
-    totalRow.appendChild(finalBalanceCell);
-    tbody.appendChild(totalRow);
-
-    // Ряд с итогами по всем услугам
-    const chargesSummaryRow = document.createElement('tr');
-    chargesSummaryRow.classList.add('itog'); 
-    const totalChargeForAllServices = Object.values(totalChargesByService).reduce((sum, value) => sum + value, 0);
-    chargesSummaryRow.innerHTML = `<td colspan="${services.size}" v="${totalChargeForAllServices.toFixed(2)}"></td>`;
-    tbody.appendChild(chargesSummaryRow);
-
-} else {
-    // Если одна услуга
-    const totalRow = document.createElement('tr');
-    totalRow.classList.add('itog'); 
-    totalRow.innerHTML = `<td align="LEFT">Итого за ${year} год</td>`;
-
-    // Итог начислений по единственной услуге
-    const totalChargeForOneService = Object.values(totalChargesByService)[0] || 0; // Получаем сумму для единственной услуги
-    totalRow.innerHTML += `<td v="${totalChargeForOneService.toFixed(2)}"></td>`;
-
-    // Итог по оплатам
-    let totalPaymentsForOneService = 0;
-    for (const month in paymentData[year]) {
-        const monthlyPayments = paymentData[year][month] || [];
-        const monthPaymentsSum = monthlyPayments.reduce((sum, payment) => sum + payment.sum, 0);
-        totalPaymentsForOneService += monthPaymentsSum;
-    }
-
-    totalRow.innerHTML += `<td v="${totalPaymentsForOneService.toFixed(2)}"></td>`;
-
-    // Общий долг/переплата на конец года
-    totalRow.innerHTML += `<td class="${cumulativeBalance > 0 ? 'red' : 'green'}" v="${cumulativeBalance.toFixed(2)}"></td>`;
-
-
-    tbody.appendChild(totalRow);
-}
- if (lastRow) tbody.appendChild(lastRow);
 
         table.appendChild(tbody);
         yearContent.appendChild(table);
@@ -231,15 +162,10 @@ if (services.size > 1) {
         container.appendChild(yearDiv);
         lastYearToggle = yearToggle; // Сохраняем чекбокс последнего года
     }
-    if (lastYearToggle) {
-        lastYearToggle.checked = true;
-    }
-
-    const curLS = ls[accountId];
-    document.getElementById('datetime').innerHTML =
-        `<br><div>
+    const curLS=ls[accountId];
+document.getElementById('datetime').innerHTML = `
+           <br><div>
                 ЛС: ${curLS.ls}<br>  <!-- Лицевой счет -->
-                ФИО: ${curLS.fio}<br>  <!-- ФИО -->
                 ${curLS.pl ? `Площадь: ${curLS.pl} м²<br>` : ''}  <!-- Площадь -->
                 ${curLS.pers ? `Жильцов: ${curLS.pers}<br>` : ''}  <!-- Количество жильцов -->
                 ${curLS.komn ? `Комнат: ${curLS.komn}<br>` : ''}  <!-- Количество комнат -->
@@ -249,8 +175,16 @@ if (services.size > 1) {
                 ${curLS.tel ? `Телефон: ${curLS.tel}<br>` : ''}  <!-- Телефон -->
                 ${curLS.note ? `Примечание: ${curLS.note}<br>` : ''}  <!-- Примечание -->
                 ${curLS.email ? `e-mail: ${curLS.email}<br>` : ''}  <!-- Email -->
-            <br>Данные указаны по состоянию на <br>${dt} (${timeAgo(dt)}назад.)
-        </div>`;
+            </div>
+ 
+    <br>Данные указаны по состоянию на <br>${dt} (${timeAgo(dt)}назад.)
+`;
+    // Устанавливаем последний год открытым
+    if (lastYearToggle) {
+        lastYearToggle.checked = true;
+    }
+    initPosters();
+    setParam('kv', ls[accountId].kv);
 }
 
 
@@ -315,9 +249,7 @@ paymentCell.innerHTML = `
 }
 function initLS() {
     document.getElementById('maincontainer').innerHTML=`
-    <div id=header><TABLE WIDTH=100%><TR><TD ALIGN=RIGHT><B>Адрес:</B></TD><TD class='big' ALIGN=LEFT><U><I><a id='adr'>adr</a><select class='big' id='number'></select></TD>
-    <td rowsplan=2><DIV id='org' ALIGN=RIGHT><td>
-    </TR><TR><TD ALIGN=RIGHT><B>Ф.И.О.:</B></TD><TD ALIGN=LEFT><U><I><div class='big' id='fio'></U></I></TD></div></TR></TABLE></DIV><DIV id='din'></div><DIV id='datetime'></div>
+    <DIV id='org' ALIGN=RIGHT></DIV><DIV id='ls' ALIGN=CENTER class='big bold'></DIV><TABLE WIDTH=100%><TR><TD ALIGN=RIGHT><B>Адрес:</B></TD><TD class='big' ALIGN=LEFT><U><I><a id='adr'>adr</a><select class='big' id='number'></select></TD></TR><TR><TD ALIGN=RIGHT><B>Ф.И.О.:</B></TD><TD ALIGN=LEFT><U><I><div class='big' id='fio'></U></I></TD></div></TR></TABLE><DIV id='din'></div><DIV id='datetime'></div>
     `;
     document.getElementById('number').addEventListener('change', function() {
         addStuff(this.value)});
