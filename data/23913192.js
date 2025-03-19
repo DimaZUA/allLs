4a1a07f4331de366a506f216c2eb2ced7f6c7ba2
DimaZUA <60350224+DimaZUA@users.noreply.github.com>
2025-03-19 10:50:13 +0200

    123

--- data/23913192.js
+++ data/23913192.js
@@ -1,6 +1,6 @@
 ﻿var oplat={};var nach={}var org='ЖК «Флора-13»'
 var adr='0'
-var dt='19.03.2025 10:47:03'
+var dt='19.03.2025 10:50:13'
 var plat=}
 var what={}
 var kto={}
--- data/23913246.js
+++ data/23913246.js
@@ -1,6 +1,6 @@
 ﻿var oplat={};var nach={}var org='ЖК «Флора-8»'
 var adr='0'
-var dt='19.03.2025 10:47:02'
+var dt='19.03.2025 10:50:12'
 var plat=}
 var what={}
 var kto={}
