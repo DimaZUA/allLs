89e9f38d5837063f287e60a78c75fc62c7d74696
DimaZUA <60350224+DimaZUA@users.noreply.github.com>
2025-03-19 11:48:04 +0200

    123

--- data/23913192.js
+++ data/23913192.js
@@ -1,6 +1,6 @@
 ﻿var oplat={};var nach={}var org='ЖК «Флора-13»'
 var adr='0'
-var dt='19.03.2025 10:57:24'
+var dt='19.03.2025 11:48:04'
 var plat=}
 var what={}
 var kto={}
--- data/23913246.js
+++ data/23913246.js
@@ -1,6 +1,6 @@
 ﻿var oplat={};var nach={}var org='ЖК «Флора-8»'
 var adr='0'
-var dt='19.03.2025 10:57:23'
+var dt='19.03.2025 11:48:03'
 var plat=}
 var what={}
 var kto={}
