7a5dba13830be5bb02479262fcef6b4a28263021
DimaZUA <DIMAZ.KHUA@gmail.com>
2025-05-13 14:11:20 +0300

    123

--- data/23007720.js
+++ data/23007720.js
@@ -2,9 +2,9 @@
 var b = {};
 var oplat={};var nach={};
 var ls=};
-var org='ЖК «Флора-12»'
+var org='ЖК "Стартер"'
 var adr='0'
-var dt='13.05.2025 14:11:14'
+var dt='13.05.2025 14:11:20'
 var plat=}
 var what={}
 var kto={}
--- data/23913192.js
+++ data/23913192.js
@@ -4,7 +4,7 @@ var oplat={};var nach={};
 var ls=};
 var org='ЖК «Флора-13»'
 var adr='0'
-var dt='12.05.2025 17:46:51'
+var dt='13.05.2025 14:11:15'
 var plat=}
 var what={}
 var kto={}
