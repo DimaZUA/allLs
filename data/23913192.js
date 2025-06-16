443a25c8032120281395cc0a83383f6d641d05b3
DimaZUA <DIMAZ.KHUA@gmail.com>
2025-06-16 12:15:39 +0300

    123

--- data/23913192.js
+++ data/23913192.js
@@ -4,7 +4,7 @@ var oplat={};var nach={};
 var ls=};
 var org='ЖК «Флора-13»'
 var adr='0'
-var dt='14.06.2025 10:50:55'
+var dt='16.06.2025 12:15:29'
 var plat=}
 var what={}
 var kto={}
--- data/23913200.js
+++ data/23913200.js
@@ -4,7 +4,7 @@ var oplat={};var nach={};
 var ls=};
 var org='ЖК «Флора-12»'
 var adr='0'
-var dt='14.06.2025 10:50:54'
+var dt='16.06.2025 12:15:28'
 var plat=}
 var what={}
 var kto={}
