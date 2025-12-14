801b3c5548cb3e8928a679fd6e02d0f0cdca8c65
DimaZUA <DIMAZ.KHUA@gmail.com>
2025-12-14 12:38:51 +0200

    123

--- js/reports.js
+++ js/reports.js
@@ -155,6 +155,7 @@ async function downloadPdfAsPng(pdfUrl) {
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
+        await new Promise(r => setTimeout(r, 50));
     }
 }
 
