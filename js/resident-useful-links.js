05b09d79c5e6c1c5472ea3a611e57b65ad5c07db
DimaZUA <DIMAZ.KHUA@gmail.com>
2026-05-26 06:56:43 +0300

    26.05.2026  6:56:43,16

---
+++ js/resident-useful-links.js
@@ -0,0 +1,70 @@
+window.RESIDENT_USEFUL_LINKS = [
+  {
+    title: "Електроенергія",
+    subtitle: "Розрахунки, платежі, покази лічильників",
+    logo: "img/hez_logo.png",
+    name: "ПрАТ “ХАРКІВЕНЕРГОЗБУТ”",
+    phones: [
+      "067-728-24-28 (безкоштовно у мережі Київстар)",
+      "073-728-24-28 (безкоштовно у мережі Lifecell)",
+      "095-728-24-28 (безкоштовно у мережі Vodafon)",
+      "057-341-39-99 (за тарифами Укртелеком)",
+      "0-800-200-413 (безкоштовно на тер. України)"
+    ],
+    links: [
+      { label: "Особистий кабінет", href: "https://my.zbutenergo.kharkov.ua/?ReturnUrl=%2Faccounts%2F13613113%2FUserMeterReads" }
+    ]
+  },
+  {
+    title: "Електроенергія",
+    subtitle: "Відключення, покази лічильників",
+    logo: "img/khoe.webp",
+    name: "АТ “Харківобленерго”",
+    phones: [
+      "057 34 24 413 (Укртелеком)",
+      "050 05 40 413 (Водафон)",
+      "067 23 40 413 (Київстар)",
+      "063 05 40 413 (Лайфселл)",
+      "0 800 508 413"
+    ],
+    links: [
+      { label: "Особистий кабінет", href: "https://pa.oblenergo.kharkiv.ua/frontend/web/index.php/uk/changed-legal-acc/control-verified-user-consumers" },
+      { label: "Передача показів лічильника", href: "https://www.oblenergo.kharkov.ua/transmissionofmeterreadings" }
+    ]
+  },
+  {
+    title: "Опалення, горяча вода",
+    subtitle: "",
+    logo: "https://www.hts.kharkov.ua/images/logo.png",
+    name: "КП «Харківські Теплові Мережі»",
+    phones: [],
+    links: [
+      { label: "Особистий кабінет", href: "https://www.hts.kharkov.ua/cabinet" },
+      { label: "Контакти", href: "https://www.hts.kharkov.ua/common_reference_book.php#start" }
+    ]
+  },
+  {
+    title: "Водопостачання та водовідведення",
+    subtitle: "",
+    logo: "https://vodokanal.kharkov.ua/img/logo_uk.svg",
+    name: "КП «Харківводоканал»",
+    phones: [],
+    links: [
+      { label: "Особистий кабінет", href: "https://billing.vodokanal.kharkov.ua" },
+      { label: "Контакти", href: "https://vodokanal.kharkov.ua/content/service_centers" }
+    ]
+  },
+  {
+    title: "Газопостачання",
+    subtitle: "",
+    logo: "https://my.gas.ua//images/logo/logo.svg",
+    name: "Нафтогаз",
+    phones: [],
+    links: [
+      { label: "Особистий кабінет", href: "https://my.gas.ua/home" },
+      { label: "E-mail", href: "mailto:client@gas.ua" },
+      { label: "Telegram", href: "https://t.me/GASUA_bot" },
+      { label: "Viber", href: "viber://pa?chatURI=naftogazpostach" }
+    ]
+  }
+];
